f226beafcaffb5134a6a986c9435d0e16481b9e0
Vlad Kolodiy <vladislavkolodiy45@gmail.com>
2025-04-05 12:43:03 +0300

    Fixed documentaion in Models.py

--- OpenAmp/src/common/model/Models.py
+++ OpenAmp/src/common/model/Models.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-#Model for table Users (for reference see SQL_init.sql)
+#Model for table Users (for reference see OpenAmp/SQL_init.sql)
 class Users(db.Model):
     __tablename__ = 'Users'
     id = db.Column(db.Integer, primary_key=True)
@@ -11,7 +11,7 @@ class Users(db.Model):
     password = db.Column(db.String(50), unique=False, nullable=False)
     role = db.Column(db.String(80), unique=False, nullable=False)
 
-#Model for table Songs (for reference see SQL_init.sql)
+#Model for table Songs (for reference see OpenAmp/SQL_init.sql)
 class Songs(db.Model):
     __tablename__ = 'Songs'
     id = db.Column(db.Integer, primary_key=True)
@@ -22,7 +22,7 @@ class Songs(db.Model):
     playlistid = db.Column(db.String(50), db.ForeignKey('playlists.id'), nullable=False)
     songuploader = db.Column(db.String(50), db.ForeignKey('user.id'), nullable=False)
 
-#Model for table Playlists (for reference see SQL_init.sql)
+#Model for table Playlists (for reference see OpenAmp/SQL_init.sql)
 class Playlists(db.Model):
     __tablename__ = 'Playlists'
     id = db.Column(db.Integer, primary_key=True)
